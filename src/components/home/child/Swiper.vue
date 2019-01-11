3d04c59336317e097ba8679161e071ae37b1c327
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-11 12:12:27 +0800

    Create Swiper.vue

---
+++ src/components/home/child/Swiper.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="swiper">
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="item in swiperList" :key="item.id">
+          <img class="swiper-img" :src="item.imgUrl" alt="">
+        </swiper-slide>
+
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+        <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
+        <!--<div class="swiper-button-next" slot="button-next"></div>-->
+        <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+      </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeSwiper',
+        data(){
+          return {
+            swiperOption:{
+              //圆点
+              pagination:'.swiper-pagination',
+
+              //按钮
+//              nextButton:'swiper-button-next',
+//              prevButton:'swiper-button-prev',
+              //滚动条
+//              scrollbar:'swiper-scrollbar',
+
+//              slidesPerView: 3,
+              autoplay:1000,
+              speed:200,
+              loop:true
+
+            },
+            swiperList:[
+              {
+                id:'0001',
+                imgUrl:'http://localhost:8080/images/s02.jpg'
+              },
+              {
+                id:'0002',
+                imgUrl:'http://localhost:8080/images/s01.jpg'
+              },
+              {
+                id:'0003',
+                imgUrl:'http://localhost:8080/images/s03.jpg'
+              },
+              {
+                id:'0004',
+                imgUrl:'http://localhost:8080/images/s04.jpg'
+              },
+              {
+                id:'0005',
+                imgUrl:'http://localhost:8080/images/s05.jpg'
+              }
+            ]
+            }
+          }
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .swiper >>> .swiper-button-next
+    background-size 20px 20px
+  .swiper >>> .swiper-pagination-bullet-active
+    background #ffffff
+  .swiper
+    overflow :hidden
+    width :100%
+    height:0
+    padding-bottom :26.66%
+    background :#eee
+    .swiper-img
+      width 100%
+</style>
