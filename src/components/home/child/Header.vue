b88ab3e7c683f455cee6164c2728f0f98ef744d8
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-11 12:11:06 +0800

    Create Header.vue

---
+++ src/components/home/child/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="header">
+      <div class="header-left">城市
+        <span class="iconfont arrow_icon">&#xe64a;</span>
+      </div>
+      <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题</div>
+      <div class="header-right">
+        <span class="iconfont info_icon">&#xe62f;</span>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeHeader',
+        data(){
+        }
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~styles/varibles.styl"
+.header
+  line-height .86rem
+  display flex
+  background $bgcolor
+  color #ffffff
+  .header-left
+    width 1.24rem
+    float left
+    text-align center
+    .arrow_icon
+      font-size .01rem
+      margin-left -.04rem
+  .header-input
+    flex 1
+    height .64rem
+    line-height .64rem
+    margin-top .12rem
+    margin-right  .2rem
+    padding-left .2rem
+    background #ffffff
+    border-radius .08rem
+    color #ccc
+  .header-right
+    width .64rem
+    float right
+    .info_icon
+      text-align center
+      font-size .5rem
+
+</style>
