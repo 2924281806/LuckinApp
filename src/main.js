29893ec26e3fc856a6d0237e96566383bf332c2a
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-11 12:01:13 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'styles/reset.css'
+import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+// require styles
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 
@@ -10,6 +16,5 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
