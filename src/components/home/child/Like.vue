73dabeee49c9fa3464eaa6579811ec84ef6553bd
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-22 10:15:36 +0800

    Update Like.vue

--- src/components/home/child/Like.vue
+++ src/components/home/child/Like.vue
@@ -1,23 +1,24 @@
 <template>
     <div class="like">
-      <div class="title">
-        <img class="title-img" src="http://localhost:8080/images/xin.png" alt="">
+        <div class="title">
+       <!--<img class="title-img" src="http://localhost:8080/images/xin.png" alt="">-->
+        <span class="title-img iconfont">&#xe61a;</span>
         <span class="title-desc">猜你喜欢</span>
       </div>
       <ul>
-        <li class="item" v-for="item of LikeList" :key="item.id">
+        <router-link tag="li" class="item" v-for="item of list" :key="item.id" :to="'/detail/' + item.id">
           <img class="item-img" :src="item.imgUrl" alt="">
           <div class="item-info">
             <p class="item-title">{{item.title}}</p>
-            <p class="item-star">2222条评论</p>
+            <p class="item-star">{{item.star}}条评论</p>
             <p class="item-price">
               <span class="item-dec">￥</span>
-              <span class="item-num">155</span>
+              <span class="item-num">{{item.price}}</span>
               起
               <span class="item-addr">{{item.addr}}</span>
             </p>
           </div>
-        </li>
+        </router-link>
       </ul>
       <div class="total">
         查看所有产品
@@ -27,78 +28,10 @@
 
 <script>
    export default {
-      name: 'HomeLike',
-      data(){
-       return {
-          LikeList:[
-            {
-              id:'0001',
-              imgUrl:'http://localhost:8080/images/r01.jpg',
-              title:'东部华侨城大峡谷',
-              addr:'东部华侨城大峡谷'
-            },
-            {
-              id:'0002',
-              imgUrl:'http://localhost:8080/images/r02.jpg',
-              title:'阿尔卑斯滑雪场',
-              addr:'世界之窗'
-            },
-            {
-              id:'0003',
-              imgUrl:'http://localhost:8080/images/r03.jpg',
-              title:'青青世界',
-              addr:'南山区'
-            },
-            {
-              id:'0004',
-              imgUrl:'http://localhost:8080/images/r04.jpg',
-              title:'深圳深蓝秘境欢乐水秀',
-              addr:'南山区'
-            },
-            {
-              id:'0005',
-              imgUrl:'http://localhost:8080/images/r05.jpg',
-              title:'华侨城',
-              addr:'南山区'
-            }
-            ,
-            {
-              id:'0006',
-              imgUrl:'http://localhost:8080/images/r06.jpg',
-              title:'深圳小梅沙海洋世界',
-              addr:'小梅沙'
-            }
-            ,
-            {
-              id:'0007',
-              imgUrl:'http://localhost:8080/images/r07.jpg',
-              title:'深圳小梅沙海洋世界',
-              addr:'小梅沙'
-            }
-            ,
-            {
-              id:'0008',
-              imgUrl:'http://localhost:8080/images/r08.jpg',
-              title:'深圳小梅沙海洋世界',
-              addr:'小梅沙'
-            }
-            ,
-            {
-              id:'0009',
-              imgUrl:'http://localhost:8080/images/r09.jpg',
-              title:'深圳小梅沙海洋世界',
-              addr:'小梅沙'
-            }
-            ,
-            {
-              id:'0010',
-              imgUrl:'http://localhost:8080/images/r10.jpg',
-              title:'深圳小梅沙海洋世界',
-              addr:'小梅沙'
-            }
-          ]
-        }
-      }
+    name: 'HomeLike',
+     props:{
+       list:Array
+     }
     }
 </script>
 
@@ -112,8 +45,7 @@
     text-indent :.1rem
     padding-left .1rem
     .title-img
-      width 15px
-      margin-top :-.2rem
+      color orangered
   .item
     overflow hidden
     display flex
@@ -131,6 +63,7 @@
       padding .1rem
       min-width 0
       .item-title
+        width 70%
         padding-top .2rem
         line-height .54rem
         font-size .32rem
@@ -155,5 +88,5 @@
     line-height .8rem
     text-align center
     color: #25a4bb
-    padding-bottom  -.4rem
+    padding-bottom  -.1rem
 </style>
