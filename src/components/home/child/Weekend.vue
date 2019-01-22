050930a48cd261d0097a9e34970e2e231e49df02
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-22 10:17:11 +0800

    Update Weekend.vue

--- src/components/home/child/Weekend.vue
+++ src/components/home/child/Weekend.vue
@@ -2,7 +2,7 @@
   <div class="like">
     <div class="title">周末去哪儿</div>
     <ul>
-      <li class="item" v-for="item of LikeList" :key="item.id">
+      <router-link  tag="li" :to="'/punch/' + item.id" class="item" v-for="item of list" :key="item.id">
         <div class="item-wrapper">
           <img class="item-img" :src="item.imgUrl" alt="">
         </div>
@@ -10,7 +10,7 @@
           <p class="item-title">{{item.title}}</p>
           <p class="item-desc">{{item.desc}}</p>
         </div>
-      </li>
+      </router-link>
     </ul>
   </div>
 </template>
@@ -18,41 +18,8 @@
 <script>
   export default {
     name: 'HomeWeekend',
-    data(){
-      return {
-        LikeList:[
-          {
-            id:'0001',
-            imgUrl:'http://localhost:8080/images/w01.jpg',
-            title:'深圳必打卡',
-            desc:'来到深圳，你必须要来这里签到'
-          },
-          {
-            id:'0002',
-            imgUrl:'http://localhost:8080/images/w02.jpg',
-            title:'粤东泡汤好去处',
-            desc:'来到深圳，你必须要来这里签到'
-          },
-          {
-            id:'0003',
-            imgUrl:'http://localhost:8080/images/w03.jpg',
-            title:'东部华侨城温泉',
-            desc:'来到深圳，你必须要来这里签到'
-          },
-          {
-            id:'0004',
-            imgUrl:'http://localhost:8080/images/w04.jpg',
-            title:'深圳小梅沙海洋世界',
-            desc:'来到深圳，你必须要来这里签到'
-          },
-          {
-            id:'0005',
-            imgUrl:'http://localhost:8080/images/w05.jpg',
-            title:'深圳小梅沙海洋世界',
-            desc:'来到深圳，你必须要来这里签到,你必须要来这里签到,你必须要来这里签到,你必须要来这里签到'
-          }
-        ]
-      }
+    props:{
+      list:Array
     }
   }
 </script>
@@ -82,7 +49,6 @@
         padding-top .1rem
         line-height .4rem
         font-size .32rem
-        font-size 14px
         ellipsis()
         width 30%
       .item-desc
