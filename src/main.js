8693e4d47bd33ca7a04ada2e085c17dd544b1c73
2924281806 <39575974+2924281806@users.noreply.github.com>
2019-01-22 14:39:41 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import FastClick from 'fastclick'
 import 'styles/reset.css'
 import 'styles/iconfont.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
@@ -9,12 +11,13 @@ import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 
 Vue.use(VueAwesomeSwiper)
-
+FastClick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
